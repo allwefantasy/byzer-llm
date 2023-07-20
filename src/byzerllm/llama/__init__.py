a64f92483e51ffb6d1123507da4df257f810ee2e
WilliamZhu <allwefantasy@gmail.com>
2023-07-20 16:18:35 +0800

    update

--- src/byzerllm/llama/__init__.py
+++ src/byzerllm/llama/__init__.py
@@ -18,8 +18,8 @@ def stream_chat(self,tokenizer,ins:str, his:List[Tuple[str,str]]=[],
     for item in his:
         his_str.append(f"{user_role}:{item[0]}")
         his_str.append(f"{assistant_role}:{item[1]}")
-
-    fin_ins = his_str.join("\n") + f"{user_role}:{ins}\n{assistant_role}:"
+    user_ins = ins if ins.startWith(f"{user_role}:") else f"{user_role}:{ins}\n{assistant_role}:"
+    fin_ins = his_str.join("\n") + user_ins
 
     tokens = tokenizer(fin_ins, return_token_type_ids=False,return_tensors="pt").to(device)
     response = self.generate(
