cea0bcce0360712c35a254708dbda73f4a0a9941
yyzhang <yuanyuan.zhang@haochezhu.club>
2023-08-17 14:58:12 +0800

    More friendly prompts

--- setup-machine/setup-machine.sh
+++ setup-machine/setup-machine.sh
@@ -390,6 +390,7 @@ EOF
             if [ $STATUS -eq 0 ]; then
                 echo "install docker.io succeeded"
             else
+                echo "change docker source to docker.com"
                 sudo apt-get update
                 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
