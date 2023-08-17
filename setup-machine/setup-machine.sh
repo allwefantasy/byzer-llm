5011f8bb9b9e67057bbcec45e07a1849ec4fa8ee
WilliamZhu <allwefantasy@gmail.com>
2023-08-17 22:07:06 +0800

    update

--- setup-machine/setup-machine.sh
+++ setup-machine/setup-machine.sh
@@ -413,7 +413,7 @@ EOF
                 sudo apt-get update
                 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-                sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+                sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
                 sudo apt-get update
                 sudo apt-get install -y docker-ce
             fi          
