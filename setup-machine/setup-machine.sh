a119e082a91570fb23928db359ffff6c259f89e3
WilliamZhu <allwefantasy@gmail.com>
2023-08-17 22:06:40 +0800

    update

--- setup-machine/setup-machine.sh
+++ setup-machine/setup-machine.sh
@@ -411,11 +411,11 @@ EOF
             else
                 echo "change docker source to docker.com"
                 sudo apt-get update
-                sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+                sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
                 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
                 sudo apt-get update
-                sudo apt-get install docker-ce
+                sudo apt-get install -y docker-ce
             fi          
         elif [ "$OS" = "centos" ]; then
             sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
