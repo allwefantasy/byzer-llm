7ecea3f31074e1b84dce83f3563e89cf64fe8bd4
WilliamZhu <allwefantasy@gmail.com>
2023-09-02 10:09:43 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
 project=byzerllm
-version=0.1.0
+
+version=$(grep -oP '__version__ = "\K[0-9]+\.[0-9]+\.[0-9]+' src/byzerllm/version.py | cut -d '"' -f 1)
+echo "Version: $version"
+
+echo "Clean dist"
 rm -rf ./dist/*
+
+echo "Build ${project}"
 pip uninstall -y ${project}
+
+echo "Build ${project} ${version}"
 python setup.py sdist bdist_wheel
 cd ./dist/
+
+echo "Install ${project} ${version}"
 pip install ${project}-${version}-py3-none-any.whl && cd -
 
 export MODE=${MODE:-"dev"}
 
 if [[ ${MODE} == "release" ]];then
+ echo "Upload ${project} ${version}"
  twine upload dist/*
 fi
