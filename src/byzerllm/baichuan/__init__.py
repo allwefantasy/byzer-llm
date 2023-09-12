5914a39ddcf5e175cbee68d6112033c72e49858e
WilliamZhu <allwefantasy@gmail.com>
2023-09-12 16:55:36 +0800

    update

--- src/byzerllm/baichuan/__init__.py
+++ src/byzerllm/baichuan/__init__.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM,GenerationConfig,BitsAndBytesConfig
 from pyjava.api.mlsql import DataServer
 import torch
 import os
@@ -30,16 +30,47 @@ def init_model(model_dir,infer_params:Dict[str,str]={},sys_conf:Dict[str,str]={}
     if not is_adaptor_model:        
         pretrained_model_dir = model_dir
 
+
     tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir,use_fast=False,
                                               add_bos_token=False, 
                                               model_max_length=4096,
                                               padding_side="right",
                                               trust_remote_code=True)
+    
+    quatization = infer_params.get("quatization", "false")
 
-    model = AutoModelForCausalLM.from_pretrained(pretrained_model_dir,trust_remote_code=True,
+    if quatization in ["4", "8", "true"]:
+        print(f"enable [{quatization}] quatization.", flush=True)
+        load_in_8bit = quatization == "8"
+        # default using int4
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=False,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+        if load_in_8bit:
+            llm_int8_threshold = infer_params.get("llm_int8_threshold", 6.0)
+            quantization_config = BitsAndBytesConfig(
+                load_in_8bit=True,
+                llm_int8_threshold=llm_int8_threshold,
+                llm_int8_skip_modules=None,
+                llm_int8_enable_fp32_cpu_offload=False,
+                llm_int8_has_fp16_weight=False,
+            )
+        model = AutoModelForCausalLM.from_pretrained(
+            pretrained_model_dir,
+            trust_remote_code=True,
+            device_map="auto",
+            quantization_config=quantization_config,
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(pretrained_model_dir,trust_remote_code=True,
                                                 device_map='auto',                                                
                                                 torch_dtype=torch.bfloat16                                                
-                                                )
+                                                )    
+    
+    model.generation_config = GenerationConfig.from_pretrained(pretrained_model_dir)
     
     if is_adaptor_model:
         from peft import PeftModel
