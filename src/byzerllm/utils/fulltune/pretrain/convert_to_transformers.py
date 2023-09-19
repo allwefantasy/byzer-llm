bd7dbdf2408284da9315f3f387ef61b015f9081c
WilliamZhu <allwefantasy@gmail.com>
2023-09-19 20:49:09 +0800

    udpate

--- src/byzerllm/utils/fulltune/pretrain/convert_to_transformers.py
+++ src/byzerllm/utils/fulltune/pretrain/convert_to_transformers.py
@@ -3,9 +3,24 @@ from typing import NoReturn
 import torch
 import os
 from transformers import WEIGHTS_NAME, CONFIG_NAME
+from transformers import AutoTokenizer
 
-def convert_deepspeed_checkpoint_to_transformers(checkpoint_dir:str, output_dir:str) -> NoReturn:
+def convert_deepspeed_checkpoint_to_transformers(model,
+                                                 tokenizer_dir:str,
+                                                 checkpoint_dir:str, 
+                                                 output_dir:str,tag=None) -> NoReturn:
     """
     Convert the model to transformers format.
+    model: you can create it like this, BaiChuanForCausalLM(BaiChuanConfig()) 
+    tokenizer_dir: here we can read and then save to the output_dir
+    checkpoint_dir: the deepspeed checkpoint
+    tag: epoch directory. for example 'Epoch-1'
     """    
-    zero_to_fp32.convert_zero_checkpoint_to_fp32_state_dict(checkpoint_dir, os.path.join(output_dir,WEIGHTS_NAME), tag=None)
+    temp_pytorch_model_file = os.path.join(output_dir,"temp",WEIGHTS_NAME)
+    zero_to_fp32.convert_zero_checkpoint_to_fp32_state_dict(checkpoint_dir, temp_pytorch_model_file, tag=None)    
+    model.load_state_dict(torch.load(temp_pytorch_model_file))
+    model.save_pretrained(output_dir)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
+    tokenizer.save_pretrained(output_dir)
+
+
