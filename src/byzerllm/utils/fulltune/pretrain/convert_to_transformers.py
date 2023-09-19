d4a175361ed7bcf97d429c0593c326727d4bcb14
WilliamZhu <allwefantasy@gmail.com>
2023-09-19 21:03:32 +0800

    update

--- src/byzerllm/utils/fulltune/pretrain/convert_to_transformers.py
+++ src/byzerllm/utils/fulltune/pretrain/convert_to_transformers.py
@@ -3,10 +3,9 @@ from typing import NoReturn
 import torch
 import os
 from transformers import WEIGHTS_NAME, CONFIG_NAME
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer,AutoModelForCausalLM
 
-def convert_deepspeed_checkpoint_to_transformers(model,
-                                                 tokenizer_dir:str,
+def convert_deepspeed_checkpoint_to_transformers(model_dir:str,                                                 
                                                  checkpoint_dir:str, 
                                                  output_dir:str,tag=None) -> NoReturn:
     """
@@ -23,10 +22,13 @@ def convert_deepspeed_checkpoint_to_transformers(model,
         os.makedirs(temp_dir)
 
     temp_pytorch_model_file = os.path.join(output_dir,"temp",WEIGHTS_NAME)
-    zero_to_fp32.convert_zero_checkpoint_to_fp32_state_dict(checkpoint_dir, temp_pytorch_model_file, tag=None)    
+    zero_to_fp32.convert_zero_checkpoint_to_fp32_state_dict(checkpoint_dir, temp_pytorch_model_file, tag=None)
+
+    model = AutoModelForCausalLM.from_pretrained(model_dir)
     model.load_state_dict(torch.load(temp_pytorch_model_file))
     model.save_pretrained(output_dir)
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_dir)
     tokenizer.save_pretrained(output_dir)
 
 
