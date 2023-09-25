45264ee7de222a4f8f058db1f1a883d1e9bcfe09
WilliamZhu <allwefantasy@gmail.com>
2023-09-25 18:15:53 +0800

    update

--- src/byzerllm/store.py
+++ src/byzerllm/store.py
@@ -41,10 +41,28 @@ async def _transfer_from_ob(udf_name, model_refs,target_dir):
         tt.close()
     os.remove(tf_path)
 
+def block_transfer_from_ob(udf_name, model_refs,target_dir):
+    tf_path = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))  
+    count = 0
+    total_count = len(model_refs)
+    with open(tf_path, "wb") as tf:
+        for item_ref in model_refs:
+            item = ray.get(item_ref)            
+            tf.write(item["value"]) 
+            if count % 1000 == 0:
+                print_flush(f"MODEL[{udf_name}] UDFWorker pull model: {float(count)/total_count*100}%")
+            count += 1  
+    with open(tf_path, "rb") as tf:
+        tt = tarfile.open(tf.name, mode="r:")
+        tt.extractall(target_dir)
+        tt.close()
+    os.remove(tf_path)        
+
 
 def transfer_from_ob(udf_name,model_refs,model_dir):
     print_flush(f"[{udf_name}] model_refs:{len(model_refs)} model_dir:{model_dir}")
     time1 = time.time()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(_transfer_from_ob(udf_name,model_refs,model_dir))            
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(_transfer_from_ob(udf_name,model_refs,model_dir))            
+    block_transfer_from_ob(udf_name,model_refs,model_dir)
     print_flush(f"[{udf_name}] UDFWorker pull model from object store cost {time.time() - time1} seconds")
