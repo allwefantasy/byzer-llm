78b7f35312e54d38cf3609405deb4805af65d336
WilliamZhu <allwefantasy@gmail.com>
2023-09-28 09:38:27 +0800

    update

--- src/byzerllm/utils/config/__init__.py
+++ src/byzerllm/utils/config/__init__.py
@@ -4,8 +4,9 @@ MLSQL_CONFIG = "__MLSQL_CONFIG__"
 
 @ray.remote
 class MLSQLConifg(object):
-    def __init__(self,json_obj):        
-        self._config = json_obj        
+    def __init__(self,instance_name:str, json_obj):        
+        self._config = json_obj
+        self.instance_name = instance_name
 
     def getitem(self, key):
         return self._config[key]
@@ -18,4 +19,4 @@ class MLSQLConifg(object):
 
 
 def create_mlsql_config(name,json_obj):
-    return MLSQLConifg.options(name=f"__MLSQL_CONFIG__{name}",lifetime="detached").remote(json_obj)
+    return MLSQLConifg.options(name=MLSQL_CONFIG,lifetime="detached").remote(json_obj)
