092fd362a2fe27120b2d0dc46e3364c49d9b1f57
WilliamZhu <allwefantasy@gmail.com>
2023-10-09 14:29:54 +0800

    update

--- src/byzerllm/records/__init__.py
+++ src/byzerllm/records/__init__.py
@@ -2,31 +2,35 @@ import dataclasses
 from typing import Optional
 
 @dataclasses.dataclass
-class ClusterSetting:
+class ClusterSettings:
     name:str
     location:str
 
-@dataclasses.dataclass
 class TableSettings:
-    database:str
-    table:Optional[str]
-    schema:str
-    location:Optional[str] 
-    num_shards:int
+    def __init__(self, database:str, table:Optional[str], schema:str, location:Optional[str], num_shards:int):
+        self.database = database
+        self.table = table
+        self.schema = schema
+        self.location = location
+        self.num_shards = num_shards    
 
-@dataclasses.dataclass
-class EnvSettings: 
-    javaHome:str
-    path:str
 
-@dataclasses.dataclass
+class EnvSettings:
+    def __init__(self, javaHome:str, path:str):
+        self.javaHome = javaHome
+        self.path = path    
+
+
 class JVMSettings:
-     options: list[str]
+    def __init__(self, options:list[str]):
+        self.options = options    
 
-@dataclasses.dataclass
+
 class SearchQuery:
-       keyword: Optional[str] 
-       fields: list[str]
-       vector: list[float]
-       vectorField: Optional[str]
-       limit: int=10
+    def __init__(self, keyword:Optional[str], fields:list[str], vector:list[float], vectorField:Optional[str], limit:int=10):
+        self.keyword = keyword
+        self.fields = fields
+        self.vector = vector
+        self.vectorField = vectorField
+        self.limit = limit
+    
