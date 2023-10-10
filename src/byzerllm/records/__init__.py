cf5ea3a55cffe67c244492f3ed7dea8f7ce16ea7
WilliamZhu <allwefantasy@gmail.com>
2023-10-10 18:22:14 +0800

    update

--- src/byzerllm/records/__init__.py
+++ src/byzerllm/records/__init__.py
@@ -1,9 +1,13 @@
 from typing import Optional
+import json
 
 class ClusterSettings:
     def __init__(self, name:str, location:str):
         self.name = name
         self.location = location    
+
+    def json(self):
+        return json.dumps(self.__dict__,ensure_ascii=False)        
 
 class TableSettings:
     def __init__(self, database:str, table:Optional[str], schema:str, location:Optional[str], num_shards:int):
@@ -11,18 +15,27 @@ class TableSettings:
         self.table = table
         self.schema = schema
         self.location = location
-        self.num_shards = num_shards    
+        self.num_shards = num_shards
+
+    def json(self):
+        return json.dumps(self.__dict__,ensure_ascii=False)    
 
 
 class EnvSettings:
     def __init__(self, javaHome:str, path:str):
         self.javaHome = javaHome
-        self.path = path    
+        self.path = path   
+
+    def json(self):
+        return json.dumps(self.__dict__,ensure_ascii=False)         
 
 
 class JVMSettings:
     def __init__(self, options:list[str]):
-        self.options = options    
+        self.options = options   
+
+    def json(self):
+        return json.dumps(self.__dict__,ensure_ascii=False)         
 
 
 class SearchQuery:
@@ -32,4 +45,8 @@ class SearchQuery:
         self.vector = vector
         self.vectorField = vectorField
         self.limit = limit
+
+    def json(self):
+        return json.dumps(self.__dict__,ensure_ascii=False)    
+        
     
---
+++ src/byzerllm/utils/retrieval/__init__.py
@@ -0,0 +1,100 @@
+
+import ray 
+from ray.types import ObjectRef
+from byzerllm.records import ClusterSettings, EnvSettings, JVMSettings, TableSettings,SearchQuery
+from typing import List,Dict,Any
+import byzerllm.utils.object_store_ref_util as ref_utils
+import json
+
+
+class ByzerRetrieval:
+    
+    def __init__(self):
+        self.launched = False
+        self.retrieval_gateway = None
+        self.clusters = {}
+
+    def launch_gateway(self)-> ray.actor.ActorHandle:
+        if self.launched:
+            return ray.get_actor("RetrievalGateway")
+        
+        retrieval_gateway_launcher_clzz = ray.cross_language.java_actor_class("tech.mlsql.retrieval.RetrievalGatewayLauncher")
+        retrieval_gateway_launcher = retrieval_gateway_launcher_clzz.remote()
+        ray.get(retrieval_gateway_launcher.launch.remote()) 
+        retrieval_gateway = ray.get_actor("RetrievalGateway")
+        self.launched = True
+        self.retrieval_gateway = retrieval_gateway
+        return retrieval_gateway  
+
+    def gateway(slef) -> ray.actor.ActorHandle:
+        return ray.get_actor("RetrievalGateway")
+    
+
+    def start_cluster(self, cluster_settings:ClusterSettings, 
+                      table_settings:TableSettings,
+                      env_settings:EnvSettings, 
+                      jvm_settings:JVMSettings)-> bool:
+        if not self.launched:
+            raise Exception("Please launch gateway first")
+        
+        obj_ref1 = self.retrieval_gateway.buildCluster.remote(
+                    cluster_settings.json(),
+                    table_settings.json(),
+                    env_settings.dumps(env_settings,default=lambda o: o.__dict__),
+                    jvm_settings.dumps(jvm_settings,default=lambda o: o.__dict__))
+        
+        return ray.get(obj_ref1) 
+    
+    def cluster(self,name:str) -> ray.actor.ActorHandle:
+        if not self.launched:
+            raise Exception("Please launch gateway first")
+        
+        if name in self.clusters:
+            return self.clusters[name]
+        
+        cluster_ref = self.retrieval_gateway.getCluster.remote(name)
+        # master_ref.buildFromRayObjectStore.remote("db1","table1",data_refs)
+        cluster = ray.get(cluster_ref)
+        self.clusters[name] = cluster
+        return cluster
+
+    def build(self, cluster_name:str, database:str, table:str, object_refs:List[ObjectRef[str]])-> bool:
+        cluster = self.cluster(cluster_name)
+        
+        data_ids = ref_utils.get_object_ids(object_refs)
+        locations = ref_utils.get_locations(object_refs)
+        return ray.get(cluster.buildFromRayObjectStore.remote(database,table,data_ids,locations)) 
+    
+    def search_keyword(self,cluster_name:str, 
+                       database:str, 
+                       table:str, 
+                       keyword:str, 
+                       fields:List[str], 
+                       limit:int=10) -> List[Dict[str,Any]]:
+        search = SearchQuery(keyword=keyword,fields=fields,vector=[],vectorField=None,limit=limit)
+        cluster = self.cluster(cluster_name)
+        v = cluster.search.remote(database,table,search.json())
+        return json.loads(ray.get(v))
+    
+    def search_vector(self,cluster_name:str, 
+                       database:str, 
+                       table:str, 
+                       vector:List[float], 
+                       vector_field:str,                        
+                       limit:int=10) -> List[Dict[str,Any]]:
+        search = SearchQuery(keyword=None,fields=[],vector=vector,vectorField=vector_field,limit=limit)
+        cluster = self.cluster(cluster_name)
+        v = cluster.search.remote(database,table,search.json())
+        return json.loads(ray.get(v))
+    
+        
+
+
+    
+    
+    
+
+        
+
+
+      
