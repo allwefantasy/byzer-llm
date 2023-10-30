0c5b60ebaffcdd211182b7df61b55aa6da9a8a71
WilliamZhu <allwefantasy@gmail.com>
2023-10-30 15:34:36 +0800

    update

--- src/byzerllm/utils/client/__init__.py
+++ src/byzerllm/utils/client/__init__.py
@@ -1,8 +1,9 @@
 from pyjava.udf import UDFMaster
-from pyjava import RayContext
+from pyjava import PythonContext,RayContext
 from typing import Dict,Any,List,Optional,Union
 from pyjava.udf import UDFBuilder
 import ray
+from ray.util.client.common import ClientActorHandle, ClientObjectRef
 import json
 import dataclasses
 
@@ -44,10 +45,81 @@ class LLMRequest:
 class ByzerLLM:
     def __init__(self,url:Optional[str]=None,**kwargs):
         self.url = url       
-        if url is not None:            
+        self.sys_conf = {}
+        
+        self.sql_model = "context" in globals()
+
+        if url is not None and self.sql_model:            
             v = globals()
             self.context = v["context"]
             self.ray_context = RayContext.connect(v, self.url, **kwargs)
+        else:
+            self.context = PythonContext(
+                0,[],self.sys_conf
+            ) 
+            self.context.have_fetched = True
+            self.ray_context = self.context.rayContext
+    
+    def setup(self,name:str, value:Any)->'ByzerLLM':
+        self.sys_conf[name]=value
+        # update the context conf
+        self.context.conf = self.sys_conf
+        return self
+
+    def deploy(self,model_path:str,
+               pretrained_model_type:str,
+               udf_name:str,
+               infer_params:Dict[str,Any]):        
+        from byzerllm import common_init_model
+        self.setup("UDF_CLIENT",udf_name)
+        model_type = pretrained_model_type
+        
+        if pretrained_model_type.startswith("saas/"):
+            model_type = pretrained_model_type.split("/")[-1]
+            import importlib            
+            infer_module = importlib.import_module(f'from byzerllm.saas.{model_type} import CustomSaasAPI')
+            from byzerllm.utils.text_generator import simple_predict_func
+            def init_model(model_refs: List[ClientObjectRef], conf: Dict[str, str]) -> Any:
+                from byzerllm import consume_model
+                consume_model(conf)                
+                infer = infer_module(infer_params)
+                return (infer,None)
+            UDFBuilder.build(self.ray_context,init_model,simple_predict_func)
+            return 
+
+
+        if pretrained_model_type == "bark":
+            from byzerllm.bark.bark_voice import build_void_infer, ZH_SPEAKER, EN_SPEAKER            
+            def init_model(model_refs: List[ClientObjectRef], conf: Dict[str, str]) -> Any:
+                infer = build_void_infer(
+                model_dir=model_path,
+                tokenizer_dir=f"{model_path}/pretrained_tokenizer")
+                return infer
+            def predict_func(model,v):
+                data = [json.loads(item) for item in v]
+                results=[{"predict":model.text_to_voice(item["instruction"]).tolist(),"labels":""} for item in data]
+                return {"value":[json.dumps(results,ensure_ascii=False,indent=4)]}
+            UDFBuilder.build(self.ray_context,init_model,predict_func)
+            return                
+        
+        if pretrained_model_type.startswith("custom/"):
+            model_type = pretrained_model_type.split("/")[-1]
+
+        predict_func = "simple_predict_func"
+        if model_type == "chatglm2":
+            predict_func = "chatglm_predict_func"
+
+        import importlib            
+        infer_module = importlib.import_module(f'byzerllm.{model_type} as infer')
+        predict_module = importlib.import_module(f"from byzerllm.utils.text_generator import {predict_func}")
+        
+        def init_model(model_refs: List[ClientObjectRef], conf: Dict[str, str]) -> Any:
+            common_init_model(model_refs,conf,model_path, is_load_from_local=True)
+            model = infer.init_model(model_path,infer_params,conf)
+            return model
+        
+        UDFBuilder.build(self.ray_context,infer_module,predict_module)
+
 
     def emb(self, model, request:LLMRequest ,extract_params:Dict[str,Any]={})->List[List[float]]:
         if isinstance(request.instruction,str):
