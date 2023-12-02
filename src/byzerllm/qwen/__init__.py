0be482337399603c839c26f52afb4de1bc1c335c
WilliamZhu <allwefantasy@gmail.com>
2023-12-02 13:22:52 +0800

    update

--- src/byzerllm/qwen/__init__.py
+++ src/byzerllm/qwen/__init__.py
@@ -67,19 +67,19 @@ def stream_chat(self,tokenizer,ins:str, his:List[Dict[str,str]]=[],
 
 
 def init_model(model_dir,infer_params:Dict[str,str]={},sys_conf:Dict[str,str]={}):
-    longContextMode = infer_params.get("longContextMode","true") == "true"    
-    if longContextMode:
-        old_init = transformers.models.llama.modeling_llama.LlamaRotaryEmbedding.__init__
-        def ntk_scaled_init(self, dim, max_position_embeddings=4096, base=10000, device=None):
+    # longContextMode = infer_params.get("longContextMode","true") == "true"    
+    # if longContextMode:
+    #     old_init = transformers.models.llama.modeling_llama.LlamaRotaryEmbedding.__init__
+    #     def ntk_scaled_init(self, dim, max_position_embeddings=4096, base=10000, device=None):
 
-            #The method is just these three lines
-            max_position_embeddings = 16384
-            a = 8 #Alpha value
-            base = base * a ** (dim / (dim-2)) #Base change formula
+    #         #The method is just these three lines
+    #         max_position_embeddings = 16384
+    #         a = 8 #Alpha value
+    #         base = base * a ** (dim / (dim-2)) #Base change formula
 
-            old_init(self, dim, max_position_embeddings, base, device)    
+    #         old_init(self, dim, max_position_embeddings, base, device)    
         
-        transformers.models.llama.modeling_llama.LlamaRotaryEmbedding.__init__ = ntk_scaled_init
+    #     transformers.models.llama.modeling_llama.LlamaRotaryEmbedding.__init__ = ntk_scaled_init
 
     pretrained_model_dir = os.path.join(model_dir,"pretrained_model")
     adaptor_model_dir = model_dir
