3d7e77fd5e905bb0d23e813fc7857b52b1ec6d6d
WilliamZhu <allwefantasy@gmail.com>
2023-12-12 15:54:56 +0800

    update

--- src/byzerllm/apps/agent/extensions/spark_sql_agent.py
+++ src/byzerllm/apps/agent/extensions/spark_sql_agent.py
@@ -1,6 +1,6 @@
 from ..conversable_agent import ConversableAgent
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
-from ....utils.client import ByzerLLM,ByzerRetrieval
+from ....utils.client import ByzerLLM,ByzerRetrieval,code_utils
 from ..agent import Agent
 import ray
 from ray.util.client.common import ClientActorHandle, ClientObjectRef
@@ -62,7 +62,8 @@ The last but most important, let me know if you have any areas of confusion. If 
         name: str,
         llm: ByzerLLM,        
         retrieval: ByzerRetrieval,        
-        code_agent: Union[Agent, ClientActorHandle,str],        
+        code_agent: Union[Agent, ClientActorHandle,str],  
+        sql_reviewer_agent: Union[Agent, ClientActorHandle,str],      
         system_message: Optional[str] = DEFAULT_SYSTEM_MESSAGE,        
         is_termination_msg: Optional[Callable[[Dict], bool]] = None,
         max_consecutive_auto_reply: Optional[int] = None,
@@ -81,8 +82,11 @@ The last but most important, let me know if you have any areas of confusion. If 
             **kwargs,
         )
 
+        self.code_agent = code_agent
+        self.sql_reviewer_agent = sql_reviewer_agent
         self._reply_func_list = []
         # self.register_reply([Agent, ClientActorHandle,str], ConversableAgent.generate_llm_reply)   
+        self.register_reply([Agent, ClientActorHandle,str], SparkSQLAgent.generate_reply_for_reviview)
         self.register_reply([Agent, ClientActorHandle,str], SparkSQLAgent.generate_sql_reply) 
         self.register_reply([Agent, ClientActorHandle,str], ConversableAgent.check_termination_and_human_reply) 
 
@@ -94,11 +98,54 @@ The last but most important, let me know if you have any areas of confusion. If 
         config: Optional[Any] = None,
         ) -> Tuple[bool, Union[str, Dict, None,ChatResponse]]:  
 
+        if get_agent_name(sender) == get_agent_name(self.sql_reviewer_agent):
+            return False,None
+        
         if messages is None:
             messages = self._messages[get_agent_name(sender)]
-                        
+
+        # give the response to    
+        _,v = self.generate_llm_reply(raw_message,messages,sender)
+        codes = code_utils.extract_code(v)
+        has_sql_code = False
+
+        for code in codes:
+            if code[0]=="sql":
+                has_sql_code = True
+
+        if has_sql_code:                
+            self.send(messages[-1],self.sql_reviewer_agent,request_reply=False)
+            self.send({
+                    "content":v
+                },self.sql_reviewer_agent)
+            
+            reply = run_agent_func(self.sql_reviewer_agent,"get_chat_messages")[get_agent_name(self.sql_reviewer_agent)][-2]        
+            return True, {"content":reply["content"],"metadata":{"TERMINATE":True}}             
+        
+        return True,  {"content":v,"metadata":{"TERMINATE":True}}
+        
+    def generate_reply_for_reviview(
+        self,
+        raw_message: Optional[Union[Dict,str,ChatResponse]] = None,
+        messages: Optional[List[Dict]] = None,
+        sender: Optional[Union[ClientActorHandle,Agent,str]] = None,
+        config: Optional[Any] = None,
+        ) -> Tuple[bool, Union[str, Dict, None,ChatResponse]]: 
+        
+        if get_agent_name(sender) != get_agent_name(self.sql_reviewer_agent):
+            return False, None
+        
+        if messages is None:
+            messages = self._messages[get_agent_name(sender)] 
+
         _,v = self.generate_llm_reply(raw_message,messages,sender)
         
-        return True, {"content":v,"metadata":{"TERMINATE":True}}
+        return True, {"content":v,"metadata":{}}
+        
+        
+
+
+
+            
 
         
