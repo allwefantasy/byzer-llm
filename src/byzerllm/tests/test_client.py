9967a56b7725ff13726b1736015be9a7a5485494
WilliamZhu <allwefantasy@gmail.com>
2023-12-19 20:48:39 +0800

    update

--- src/byzerllm/tests/test_client.py
+++ src/byzerllm/tests/test_client.py
@@ -1,7 +1,36 @@
-from byzerllm.utils.client import ByzerLLM
+from byzerllm.utils.client import ByzerLLM,InferBackend,Templates
+import ray
 import pytest
 
-def test_byzerllm():
-    llm = ByzerLLM()
-    assert llm.verbose == False
-   
+model_path = "/home/winubuntu/models/Qwen-1_8B-Chat"
+
+class TestByzerLLM(object):
+    llm = None
+
+    def setup_class(self):
+        ray.init(address="auto",namespace="default")
+        self.llm = ByzerLLM()
+
+        if self.llm.is_model_exist("chat"):
+            self.llm.undeploy("chat")
+
+        self.llm.setup_gpus_per_worker(1).setup_num_workers(1).setup_infer_backend(InferBackend.VLLM)
+        self.llm.deploy(
+            model_path=model_path,
+            pretrained_model_type="custom/auto",
+            udf_name="chat",
+            infer_params={"backend.max_num_batched_tokens":8192,"backend.max_model_len":8192}
+        )
+        self.llm.setup_default_model_name("chat")
+        self.llm.setup_template("chat",Templates.qwen())
+        
+        
+    def teardown_class(self):
+        pass
+    
+    def test_chat_oai(self):
+        t = self.llm.chat_oai([{
+            "content":"你好",
+            "role":"user"
+        }])
+        print(t)
