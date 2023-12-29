974691cf7814bc8487d20fecdea6141d17ab2ffd
WilliamZhu <allwefantasy@gmail.com>
2023-12-29 15:29:39 +0800

    Update stream_chat method parameters and return value

--- src/byzerllm/saas/baichuan/__init__.py
+++ src/byzerllm/saas/baichuan/__init__.py
@@ -45,44 +45,63 @@ class CustomSaasAPI:
     
     def stream_chat(self, tokenizer, ins: str, his: List[Dict[str, Any]] = [],
                     max_length: int = 4096,
-                    top_p: float = 0.7,
-                    temperature: float = 0.9, **kwargs):
+                    top_p: float = 0.9,
+                    temperature: float = 0.1, **kwargs):
         
         messages = his + [{"role": "user", "content": ins}]
 
+        other_params = {}
+        if "with_search_enhance" in kwargs:
+            other_params["with_search_enhance"] = kwargs["with_search_enhance"]
+        
+        if "top_k" in kwargs:
+            other_params["top_k"] = kwargs["top_k"]
+
         data = {
             "model": self.model,
-            "messages": messages
-        }
+            "messages": messages,
+            "temperature": temperature,
+            "top_p": top_p,
+            "stream": False
+            **other_params
+        }        
+        
+        start_time = time.monotonic()
+        res_data = self.request_with_retry(data)   
+        time_cost = time.monotonic() - start_time
+        generated_text = res_data["choices"][0]["message"]["content"] 
 
-        content = None
+        generated_tokens_count = res_data["usage"]["completion_tokens"]   
 
-        try:
-            content = self.request_with_retry(data)
-        except Exception as e:
-            traceback.print_exc()
-            if content == "" or content is None:
-                content = f"exception occurred during the request, please check the error code: {e}"
-        return [(content, "")]
+        return [(generated_text,{"metadata":{
+        "request_id":res_data["id"],
+        "input_tokens_count":res_data["usage"]["prompt_tokens"],
+        "generated_tokens_count":generated_tokens_count,
+        "time_cost":time_cost,
+        "first_token_time":0,
+        "speed":float(generated_tokens_count)/time_cost*1000,        
+    }})]                 
+        
 
-    @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=10)
+    @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=3)
     def request_with_retry(self, data):
         json_data = json.dumps(data)
-        time_stamp = int(time.time())
-        signature = self.calculate_md5(self.secret_key + json_data + str(time_stamp))
+        # time_stamp = int(time.time())
+        # signature = self.calculate_md5(self.secret_key + json_data + str(time_stamp))
         headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + self.api_key,
-            "X-BC-Timestamp": str(time_stamp),
-            "X-BC-Signature": signature,
-            "X-BC-Sign-Algo": "MD5",
+            # "X-BC-Timestamp": str(time_stamp),
+            # "X-BC-Signature": signature,
+            # "X-BC-Sign-Algo": "MD5",
         }
         response = requests.post(self.api_url, data=json_data, headers=headers)
         if response.status_code == 200:
-            print("request baichuan api success")
-            print("response text: " + response.text)
+            # id = response.headers.get("X-BC-Request-Id")
+            
             res_data = json.loads(response.text)
-            if res_data["code"] != 0:
+            
+            if "code" in res_data and  res_data["code"] != 0:
                 if str(res_data["code"]) in BaiChuanErrorCodes.keys():
                     msg = BaiChuanErrorCodes.get(str(res_data["code"]))
                     print("request baichuan api failed, err msg:" + msg)
@@ -94,9 +113,11 @@ class CustomSaasAPI:
                     else:
                         return msg
                 # unexpected error code, retry anyway
-                raise Exception("request baichuan api failed, api response code:" + str(res_data["code"]))
-            content = res_data["data"]["messages"][0]["content"].replace(' .', '.').strip()
-            return content
+                raise Exception("request baichuan api failed, api response code:" + str(res_data["code"]))                        
+        
+            return res_data
+
+
         else:
             print("request baichuan api failed, http response code:" + str(response.status_code))
             print("response text:" + response.text)
