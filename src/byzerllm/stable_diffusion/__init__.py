f78779a2ebee52d139a1bc2bec53072ea5da04dc
WilliamZhu <allwefantasy@gmail.com>
2024-01-06 10:56:17 +0800

    Add get_meta method to model class stable_diffusion

--- src/byzerllm/stable_diffusion/__init__.py
+++ src/byzerllm/stable_diffusion/__init__.py
@@ -16,6 +16,15 @@ from byzerllm.stable_diffusion.utils import b642img
 
 # model_name = "runwayml/stable-diffusion-v1-5"
 
+def get_meta(self): 
+    config = self.config   
+    return [{
+        "model_deploy_type": "proprietary",
+        "backend":"transformers",
+        "max_model_len":getattr(config, "model_max_length", -1),
+        "architectures":getattr(config, "architectures", []),
+        "message_format":True,
+    }]
 
 def stream_chat(
     self,
@@ -102,6 +111,7 @@ def init_model(
     import types
 
     model.stream_chat = types.MethodType(stream_chat, model)
+    model.get_meta = types.MethodType(get_meta, model)
     return (model, None)
 
 
