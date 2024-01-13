f3968492f80268f9baa86937bb3179f0aeb68b62
WilliamZhu <allwefantasy@gmail.com>
2024-01-13 23:25:50 +0800

    update

--- src/byzerllm/apps/agent/extensions/spark_sql_agent.py
+++ src/byzerllm/apps/agent/extensions/spark_sql_agent.py
@@ -266,28 +266,43 @@ new_query: {new_query}
             # old_content = m["content"]
             # if time_msg:
             #     m["content"] = f'''补充信息：{time_msg} \n原始问题：{old_content} ''' 
-            time_msg = ""                         
-            now = datetime.now().strftime("%Y-%m-%d")            
-            class TimeRange(pydantic.BaseModel):
-                '''
-                时间区间
-                格式需要如下： yyyy-MM-dd
-                '''  
-                
-                start: str = pydantic.Field(...,description="开始时间.时间格式为 yyyy-MM-dd")
-                end: str = pydantic.Field(...,description="截止时间.时间格式为 yyyy-MM-dd") 
-
-            t = self.llm.chat_oai(conversations=[{
-                "content":f'''当前时间是 {now}。根据用户的问题，计算时间区间。时间格式为 yyyy-MM-dd。用户的问题是：{m["content"]}''',
-                "role":"user"
-            }],response_class=TimeRange)
+            time_msg = ""  
             
-            if t[0].value and t[0].value.start and t[0].value.end:
-                time_range:TimeRange = t[0].value
-                time_msg = f'''时间区间是：{time_range.start} 至 {time_range.end}'''  
-                print(f'compute the time range:{m["content"]}\n\n',flush=True)
+            class Item(pydantic.BaseModel):
+                '''
+                查询参数    
+                如果对应的参数不符合字段要求，那么设置为空即可
+                '''
+                other: str = pydantic.Field(...,description="其他参数，比如用户的名字，或者其他的一些信息")
+                time:  str = pydantic.Field(...,description="时间信息,比如内容里会提到天， 月份，年等相关词汇")
+
+
+            t = self.llm.chat_oai([{
+                "content":f'''{m["content"]}''',
+                "role":"user"    
+            }],response_class=Item) 
+
+            if t[0].value and t[0].value.time:                                     
+                now = datetime.now().strftime("%Y-%m-%d")            
+                class TimeRange(pydantic.BaseModel):
+                    '''
+                    时间区间
+                    格式需要如下： yyyy-MM-dd
+                    '''  
+                    
+                    start: str = pydantic.Field(...,description="开始时间.时间格式为 yyyy-MM-dd")
+                    end: str = pydantic.Field(...,description="截止时间.时间格式为 yyyy-MM-dd") 
+
+                t = self.llm.chat_oai(conversations=[{
+                    "content":f'''当前时间是 {now}。根据用户的问题，计算时间区间。时间格式为 yyyy-MM-dd。用户的问题是：{t[0].value.time}''',
+                    "role":"user"
+                }],response_class=TimeRange)
                 
-                
+                if t[0].value and t[0].value.start and t[0].value.end:
+                    time_range:TimeRange = t[0].value
+                    time_msg = f'''时间区间是：{time_range.start} 至 {time_range.end}'''  
+                    print(f'compute the time range:{m["content"]}\n\n',flush=True)
+                                
 
             key_msg = ""
             ## extract key messages is the user want to generate sql code
