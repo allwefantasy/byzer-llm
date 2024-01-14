904441af7160dac1b4cea0f83b5963976385ba17
WilliamZhu <allwefantasy@gmail.com>
2024-01-14 22:09:44 +0800

    update

--- src/byzerllm/apps/agent/extensions/sql_reviewer_agent.py
+++ src/byzerllm/apps/agent/extensions/sql_reviewer_agent.py
@@ -1,10 +1,11 @@
 from ..conversable_agent import ConversableAgent
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
-from ....utils.client import ByzerLLM,message_utils
+from ....utils.client import ByzerLLM,message_utils,code_utils
 from byzerllm.utils.retrieval import ByzerRetrieval
 from ..agent import Agent
 from ray.util.client.common import ClientActorHandle, ClientObjectRef
 from .. import get_agent_name,run_agent_func,ChatResponse
+import json
 try:
     from termcolor import colored
 except ImportError:
@@ -14,18 +15,24 @@ except ImportError:
     
 class SQLReviewerAgent(ConversableAgent): 
     DEFAULT_SYSTEM_MESSAGE='''你的任务是检查用户发送给你的Spark SQL语句。
+请仔细阅读用户发给你 Spark SQL，具体请按如下方式步骤进行检查，务必一步一步来：
 
-请仔细阅读用户发给你 Spark SQL ，按下面的约束进行逐条检查：
-
-1. SQL 中的所有字段和别名必须都使用 `` 括起来了                                          
+1. 仔细检查SQL中的每一个字段和别名是否都使用反引号（backtick）或者反撇号（grave accent）`括起来。
 2. SQL中不允许提到将一些变量替换成用户输入的内容，或者需要用户手动输入一些内容。
 3. SQL中不允许出现诸如 ?, ?, ..., ? 这种参数化查询
 4. SQL中不需有诸如 "有取 M 条记录， Top N类" 这种描述性的内容
 
-注意：不要尝试生成任何代码去解决问题，你需要根据你的知识和经验，对用户发给你的代码进行审查。
+请输出每一步检查结果，并说明原因。  
                                           
-如果违反了上述任何一个要求，你需要告诉用户，对应的SQL语句是具体不符合哪个要求。
-如果符合上述所有要求，可以说：我觉得当前的代码没有什么问题了。
+最后，对上面的检查结果和原因重新以json数组格式输出：
+
+```json
+[{
+    id： 检查步骤序号,
+    pass: 是否通过，true/false,
+    reason: 原因                                    
+}]                      
+```
 '''
     def __init__(
         self,
@@ -70,7 +77,40 @@ class SQLReviewerAgent(ConversableAgent):
 
         message = messages[-1]                
         v = self.llm.chat_oai(conversations=message_utils.padding_messages_merge(self._system_message + messages[-1:]))
-        new_message = {"content":v[0].output,"metadata":{}}        
+        checks = json.loads(code_utils.extract_code(v[0].output)[-1][1])
+
+        c = []
+        for check in checks:
+            if not check["pass"]:
+                c.append(check["reason"])
+
+        if len(c) > 0:
+            t = "\n".join(c)
+            new_message = {"content":f'''代码存在一些问题，具体的问题如下：\n{t}''',"metadata":{}}
+            return True, message_utils.copy_error_count(message,new_message)
+        
+        t = self.llm.chat_oai(conversations=[
+    {"role":"user",
+    "content":f'''仔细检查下面的 Spark SQL：
+
+{message["content"]}
+
+请找出所有的字段或者别名，不要带函数的，并将他们按照出现顺序，以json数组格式输出：
+
+```json
+[
+  "字段或者别名"
+]
+```
+'''}])
+        fields = json.loads(code_utils.extract_code(v[0].output)[-1][1])
+        for field in fields:
+            if "`" not in field:
+                new_message = {"content":f'''代码存在问题，字段或者别名: {field} 没有被反引号括起来,请修改''',"metadata":{}}
+                return True, message_utils.copy_error_count(message,new_message)
+        
+        new_message = {"content":f'''代码没有问题，可以正常允许。''',"metadata":{}}                        
+                     
         return True, message_utils.copy_error_count(message,new_message)
 
         
