2e01b39ddc50e781b147cef5d2618feca738dc2d
WilliamZhu <allwefantasy@gmail.com>
2024-05-14 13:33:34 +0800

    update

--- src/byzerllm/apps/byzer_sql/__init__.py
+++ src/byzerllm/apps/byzer_sql/__init__.py
@@ -3,6 +3,88 @@ from dataclasses import asdict
 from byzerllm.utils.client import ByzerLLM
 import json
 
+import byzerllm
+import os
+from loguru import logger
+from typing import Optional
+from os.path import expanduser
+from byzerllm.apps.llama_index import get_service_context,get_storage_context
+from pydantic import BaseModel
+from llama_index.core.service_context import ServiceContext
+from llama_index.core.storage import StorageContext
+from byzerllm import ByzerLLM,ByzerRetrieval
+from dataclasses import dataclass
+
+@dataclass
+class Env:
+    llm:ByzerLLM
+    retrieval:Optional[ByzerRetrieval] = None
+    namespace:str    
+    default_storage_context:Optional[StorageContext] = None
+    default_service_context: ServiceContext   
+     
+
+def _connect(
+        default_model:str,
+        default_emb_model:str,        
+        ray_address:str="auto",
+        base_dir:Optional[str]=None,
+        storage_version:Optional[str]=None):
+
+    version = storage_version or "0.1.11"        
+    home = expanduser("~")        
+    base_dir = base_dir or os.path.join(home, ".auto-coder")
+
+    storage_enabled = True
+
+    if not os.path.exists(base_dir):
+        logger.info(f"Byzer Storage not found in {base_dir}")
+        base_dir = os.path.join(home, ".byzerllm")
+
+    if not os.path.exists(base_dir):
+        logger.info(f"Byzer Storage not found in {base_dir}")
+        storage_enabled = False
+
+    code_search_path = None        
+    if storage_enabled and default_emb_model:
+        logger.info(f"Byzer Storage found in {base_dir}")            
+        libs_dir = os.path.join(base_dir, "storage", "libs", f"byzer-retrieval-lib-{version}")        
+        code_search_path = [libs_dir]
+        logger.info(f"Connect and start Byzer Retrieval version {version}") 
+
+    if storage_enabled and not default_emb_model:
+        logger.warning("Byzer Storage is detected but default_emb_model is not set, The storage will be disabled.")           
+        storage_enabled = False
+
+    env_vars = byzerllm.connect_cluster(address=ray_address,code_search_path=code_search_path)
+    
+    retrieval = None
+    if storage_enabled:
+        retrieval = ByzerRetrieval()
+        retrieval.launch_gateway()
+        
+    llm = byzerllm.ByzerLLM()
+    if default_model:
+        llm.setup_default_model_name(default_model)
+    if default_emb_model:    
+        llm.setup_default_emb_model_name(default_emb_model)
+    return llm,retrieval
+
+def prepare_env(default_model:Optional[str]=None,
+                default_emb_model:Optional[str]=None,
+                namespace:str="default",**kargs)->Env:
+    llm,retrieval = _connect(default_model=default_model,default_emb_model=default_emb_model,**kargs)    
+    service_context = get_service_context(llm=llm)
+    default_storage_context = None
+    if retrieval:
+        default_storage_context = get_storage_context(llm=llm,retrieval=retrieval,chunk_collection=namespace,namespace=namespace)
+    
+    return Env(llm=llm,
+            retrieval=retrieval,
+            namespace=namespace,
+            default_storage_context=default_storage_context,
+            default_service_context=service_context)
+
 def chat(ray_context):   
     conf = ray_context.conf()
     udf_name = conf["UDF_CLIENT"] 
