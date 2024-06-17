1f7d0b37ac2f9b5f3c80ed784be79917f9bcff33
WilliamZhu <allwefantasy@gmail.com>
2024-06-17 20:40:29 +0800

    auto_coder_pre_021_chat_action.yml_e2903bdb5fe1e7ffdce103b1fb0053b4

--- src/byzerllm/byzerllm_command.py
+++ src/byzerllm/byzerllm_command.py
@@ -1,137 +1,155 @@
-import argparse
 import os
 import ray
-import shlex
 import jinja2
 import yaml
+from typing import List, Optional
 from byzerllm.utils.client import ByzerLLM, InferBackend
 from byzerllm.utils.client.types import Templates
 from byzerllm.apps.byzer_storage.command import StorageSubCommand
 from byzerllm.utils.client.entrypoints.openai.serve import serve, ServerArgs
 import byzerllm
-from byzerllm.lang import locales, lang 
+from byzerllm.lang import locales, lang
 from byzerllm.command_args import StoreNestedDict, get_command_args
 
-def main():
-    args = get_command_args()
+
+def main(input_args: Optional[List[str]] = None):
+    args = get_command_args(input_args=input_args)
 
     if args.file:
         with open(args.file, "r") as f:
             config = yaml.safe_load(f)
             for key, value in config.items():
-                if key != "file":  # 排除 --file 参数本身   
+                if key != "file":  # 排除 --file 参数本身
                     ## key: ENV {{VARIABLE_NAME}}
-                    if isinstance(value, str) and value.startswith("ENV"):  
-                        template = jinja2.Template(value.removeprefix("ENV").strip())                  
-                        value = template.render(os.environ)                        
+                    if isinstance(value, str) and value.startswith("ENV"):
+                        template = jinja2.Template(value.removeprefix("ENV").strip())
+                        value = template.render(os.environ)
                     setattr(args, key, value)
-    
+
     print("Command Line Arguments:")
     print("-" * 50)
-    for arg, value in vars(args).items():        
-        if arg == "infer_params" and isinstance(value, dict) and "saas.api_key" in value:  
+    for arg, value in vars(args).items():
+        if (
+            arg == "infer_params"
+            and isinstance(value, dict)
+            and "saas.api_key" in value
+        ):
             import copy
+
             new_value = copy.deepcopy(value)
             new_value["saas.api_key"] = "******"
             print(f"{arg:20}: {new_value}")
-        else:     
+        else:
             print(f"{arg:20}: {value}")
-    print("-" * 50)    
+    print("-" * 50)
 
-    if args.command == 'deploy':
+    if args.command == "deploy":
         byzerllm.connect_cluster(address=args.ray_address)
         llm = ByzerLLM()
         if llm.is_model_exist(args.model):
             print(locales["already_deployed"][lang].format(args.model))
             return
-        
-        llm.setup_gpus_per_worker(args.gpus_per_worker).setup_cpus_per_worker(args.cpus_per_worker).setup_num_workers(args.num_workers)
+
+        llm.setup_gpus_per_worker(args.gpus_per_worker).setup_cpus_per_worker(
+            args.cpus_per_worker
+        ).setup_num_workers(args.num_workers)
         llm.setup_worker_concurrency(args.worker_concurrency)
         if not args.pretrained_model_type.startswith("saas"):
             if args.infer_backend == "vllm":
                 llm.setup_infer_backend(InferBackend.VLLM)
-            elif args.infer_backend == "llama_cpp": 
+            elif args.infer_backend == "llama_cpp":
                 llm.setup_infer_backend(InferBackend.LLAMA_CPP)
             elif args.infer_backend == "transformers":
                 llm.setup_infer_backend(InferBackend.Transformers)
-            elif args.infer_backend == "deepspeed":    
+            elif args.infer_backend == "deepspeed":
                 llm.setup_infer_backend(InferBackend.DeepSpeed)
             else:
                 raise ValueError("Invalid infer_backend")
-        
-        llm.deploy(model_path=args.model_path,
-                pretrained_model_type=args.pretrained_model_type,
-                udf_name=args.model,
-                infer_params=args.infer_params or {})
+
+        llm.deploy(
+            model_path=args.model_path,
+            pretrained_model_type=args.pretrained_model_type,
+            udf_name=args.model,
+            infer_params=args.infer_params or {},
+        )
 
         print(locales["deploy_success"][lang].format(args.model))
 
-    elif args.command == 'query':
+    elif args.command == "query":
         byzerllm.connect_cluster(address=args.ray_address)
 
-        llm_client = ByzerLLM()        
+        llm_client = ByzerLLM()
         if args.template == "default":
             llm_client.setup_template(args.model, template=Templates.default())
         elif args.template == "llama":
-            llm_client.setup_template(args.model, template=Templates.llama())    
+            llm_client.setup_template(args.model, template=Templates.llama())
         elif args.template == "qwen":
-            llm_client.setup_template(args.model, template=Templates.qwen())        
+            llm_client.setup_template(args.model, template=Templates.qwen())
         elif args.template == "yi":
-            llm_client.setup_template(args.model, template=Templates.yi())  
+            llm_client.setup_template(args.model, template=Templates.yi())
         elif args.template == "empty":
-            llm_client.setup_template(args.model, template=Templates.empty())          
+            llm_client.setup_template(args.model, template=Templates.empty())
         else:
             llm_client.setup_template(args.model, args.template)
 
-        resp = llm_client.chat_oai(model=args.model, conversations=[{
-            "role": "user",
-            "content": args.query,
-        }])
-        
+        resp = llm_client.chat_oai(
+            model=args.model,
+            conversations=[
+                {
+                    "role": "user",
+                    "content": args.query,
+                }
+            ],
+        )
+
         if args.output_file:
             ext = os.path.splitext(args.output_file)[1]
             if ext == ".mp3" or ext == ".wav":
                 import base64
+
                 with open(args.output_file, "wb") as f:
                     f.write(base64.b64decode(resp[0].output))
-            else:        
+            else:
                 with open(args.output_file, "w") as f:
                     f.write(resp[0].output)
-        else:        
-            print(resp[0].output,flush=True)
+        else:
+            print(resp[0].output, flush=True)
 
-    elif args.command == 'undeploy':
+    elif args.command == "undeploy":
         byzerllm.connect_cluster(address=args.ray_address)
 
         llm = ByzerLLM()
-        llm.undeploy(args.model,force=args.force)
+        llm.undeploy(args.model, force=args.force)
 
         print(locales["undeploy_success"][lang].format(args.model))
 
-    elif args.command == 'stat':
+    elif args.command == "stat":
         import json
+
         byzerllm.connect_cluster(address=args.ray_address)
         model = ray.get_actor(args.model)
         v = ray.get(model.stat.remote())
-        o = json.dumps(v, indent=4,ensure_ascii=False)
-        print(o)    
-        
-    elif args.command == 'storage':
+        o = json.dumps(v, indent=4, ensure_ascii=False)
+        print(o)
+
+    elif args.command == "storage":
         if args.storage_command == "install":
             StorageSubCommand.install(args)
         elif args.storage_command == "start":
-            StorageSubCommand.start(args) 
+            StorageSubCommand.start(args)
         elif args.storage_command == "stop":
             StorageSubCommand.stop(args)
         elif args.storage_command == "export":
             StorageSubCommand.export(args)
         elif args.storage_command == "collection":
-            StorageSubCommand.collection(args)    
-    
-    elif args.command == 'serve':
-        byzerllm.connect_cluster(address=args.ray_address) 
+            StorageSubCommand.collection(args)
+
+    elif args.command == "serve":
+        byzerllm.connect_cluster(address=args.ray_address)
         llm_client = ByzerLLM()
         if args.served_model_name:
-            llm_client.setup_template(args.served_model_name, args.template)                 
-        server_args = ServerArgs(**{arg: getattr(args, arg) for arg in vars(ServerArgs())})                      
-        serve(llm=llm_client, args=server_args)            
+            llm_client.setup_template(args.served_model_name, args.template)
+        server_args = ServerArgs(
+            **{arg: getattr(args, arg) for arg in vars(ServerArgs())}
+        )
+        serve(llm=llm_client, args=server_args)
--- src/byzerllm/command_args.py
+++ src/byzerllm/command_args.py
@@ -1,6 +1,8 @@
 import argparse
 import shlex
-from byzerllm.lang import lang,locales
+from byzerllm.lang import lang, locales
+from typing import List, Optional
+
 
 class StoreNestedDict(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -8,106 +10,189 @@ class StoreNestedDict(argparse.Action):
         for kv in values:
             try:
                 for clean_kv in shlex.split(kv):
-                    key, value =clean_kv.split("=", 1)
-                    d[key]=value
+                    key, value = clean_kv.split("=", 1)
+                    d[key] = value
             except ValueError:
                 raise argparse.ArgumentError(self, f"Invalid argument format: {kv}")
-        setattr(namespace, self.dest, d)  
+        setattr(namespace, self.dest, d)
 
-def get_command_args():
+
+def get_command_args(input_args: Optional[List[str]] = None):
     parser = argparse.ArgumentParser(description=locales["desc"][lang])
-    subparsers = parser.add_subparsers(dest='command')
+    subparsers = parser.add_subparsers(dest="command")
 
     # Deploy 子命令
-    deploy_cmd = subparsers.add_parser('deploy', help=locales["help_deploy"][lang])
-    deploy_cmd.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    deploy_cmd.add_argument('--num_workers', type=int, default=1, help=locales["help_num_workers"][lang])
-    deploy_cmd.add_argument('--worker_concurrency', type=int, default=1, help=locales["help_worker_concurrency"][lang])
-    deploy_cmd.add_argument('--gpus_per_worker', type=float, default=0, help=locales["help_gpus_per_worker"][lang])
-    deploy_cmd.add_argument('--cpus_per_worker', type=float, default=0.01, help=locales["help_cpus_per_worker"][lang])
-    deploy_cmd.add_argument('--model_path', default="",required=False, help=locales["help_model_path"][lang])
-    deploy_cmd.add_argument('--pretrained_model_type', default="custom/llama2", help=locales["help_pretrained_model_type"][lang])
-    deploy_cmd.add_argument('--model', required=True, help=locales["help_udf_name"][lang])    
-    deploy_cmd.add_argument('--infer_backend', default="", help=locales["help_infer_backend"][lang])
-    deploy_cmd.add_argument('--infer_params', nargs='+', action=StoreNestedDict, help=locales["help_infer_params"][lang])
+    deploy_cmd = subparsers.add_parser("deploy", help=locales["help_deploy"][lang])
+    deploy_cmd.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    deploy_cmd.add_argument(
+        "--num_workers", type=int, default=1, help=locales["help_num_workers"][lang]
+    )
+    deploy_cmd.add_argument(
+        "--worker_concurrency",
+        type=int,
+        default=1,
+        help=locales["help_worker_concurrency"][lang],
+    )
+    deploy_cmd.add_argument(
+        "--gpus_per_worker",
+        type=float,
+        default=0,
+        help=locales["help_gpus_per_worker"][lang],
+    )
+    deploy_cmd.add_argument(
+        "--cpus_per_worker",
+        type=float,
+        default=0.01,
+        help=locales["help_cpus_per_worker"][lang],
+    )
+    deploy_cmd.add_argument(
+        "--model_path",
+        default="",
+        required=False,
+        help=locales["help_model_path"][lang],
+    )
+    deploy_cmd.add_argument(
+        "--pretrained_model_type",
+        default="custom/llama2",
+        help=locales["help_pretrained_model_type"][lang],
+    )
+    deploy_cmd.add_argument(
+        "--model", required=True, help=locales["help_udf_name"][lang]
+    )
+    deploy_cmd.add_argument(
+        "--infer_backend", default="", help=locales["help_infer_backend"][lang]
+    )
+    deploy_cmd.add_argument(
+        "--infer_params",
+        nargs="+",
+        action=StoreNestedDict,
+        help=locales["help_infer_params"][lang],
+    )
     deploy_cmd.add_argument("--file", default=None, required=False, help="")
-    
+
     # Undeploy 子命令
-    deploy_cmd = subparsers.add_parser('undeploy', help=locales["help_deploy"][lang])
-    deploy_cmd.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    deploy_cmd.add_argument('--model', required=True, help=locales["help_udf_name"][lang])    
+    deploy_cmd = subparsers.add_parser("undeploy", help=locales["help_deploy"][lang])
+    deploy_cmd.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    deploy_cmd.add_argument(
+        "--model", required=True, help=locales["help_udf_name"][lang]
+    )
     deploy_cmd.add_argument("--file", default=None, required=False, help="")
-    deploy_cmd.add_argument("--force",action='store_true', help="")
+    deploy_cmd.add_argument("--force", action="store_true", help="")
 
     # Query 子命令
-    query_cmd = subparsers.add_parser('query', help=locales["help_query"][lang])
-    query_cmd.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    query_cmd.add_argument('--model', required=True, help=locales["help_query_model"][lang])
-    query_cmd.add_argument('--query', required=True, help=locales["help_query_text"][lang])
-    query_cmd.add_argument('--template', default="auto", help=locales["help_template"][lang])
+    query_cmd = subparsers.add_parser("query", help=locales["help_query"][lang])
+    query_cmd.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    query_cmd.add_argument(
+        "--model", required=True, help=locales["help_query_model"][lang]
+    )
+    query_cmd.add_argument(
+        "--query", required=True, help=locales["help_query_text"][lang]
+    )
+    query_cmd.add_argument(
+        "--template", default="auto", help=locales["help_template"][lang]
+    )
     query_cmd.add_argument("--file", default=None, required=False, help="")
     query_cmd.add_argument("--output_file", default="", help="")
-    
+
     # Stat Command
-    status_cmd = subparsers.add_parser('stat', help=locales["help_query"][lang])
-    status_cmd.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    status_cmd.add_argument('--model', required=True, help=locales["help_query_model"][lang])        
-    status_cmd.add_argument("--file", default=None, required=False, help="")    
+    status_cmd = subparsers.add_parser("stat", help=locales["help_query"][lang])
+    status_cmd.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    status_cmd.add_argument(
+        "--model", required=True, help=locales["help_query_model"][lang]
+    )
+    status_cmd.add_argument("--file", default=None, required=False, help="")
 
     # Storage 子命令
-    storage_cmd = subparsers.add_parser('storage', help='Manage Byzer Storage')    
-    storage_cmd_subparsers = storage_cmd.add_subparsers(dest='storage_command')
-    
+    storage_cmd = subparsers.add_parser("storage", help="Manage Byzer Storage")
+    storage_cmd_subparsers = storage_cmd.add_subparsers(dest="storage_command")
+
     # install 子命令
-    storage_install_cmd = storage_cmd_subparsers.add_parser('install', help='Install Byzer Storage')
+    storage_install_cmd = storage_cmd_subparsers.add_parser(
+        "install", help="Install Byzer Storage"
+    )
     storage_install_cmd.add_argument("--file", default=None, required=False, help="")
-    storage_install_cmd.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    storage_install_cmd.add_argument("--version", default="0.1.11", required=False, help="")
-    storage_install_cmd.add_argument('--cluster', default="byzerai_store", help="")
-    storage_install_cmd.add_argument('--base_dir', default="", help="")
+    storage_install_cmd.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    storage_install_cmd.add_argument(
+        "--version", default="0.1.11", required=False, help=""
+    )
+    storage_install_cmd.add_argument("--cluster", default="byzerai_store", help="")
+    storage_install_cmd.add_argument("--base_dir", default="", help="")
 
-    storage_collection_cmd = storage_cmd_subparsers.add_parser('collection', help='')
+    storage_collection_cmd = storage_cmd_subparsers.add_parser("collection", help="")
     storage_collection_cmd.add_argument("--file", default=None, required=False, help="")
-    storage_collection_cmd.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    storage_collection_cmd.add_argument("--version", default="0.1.11", required=False, help="")
-    storage_collection_cmd.add_argument('--cluster', default="byzerai_store", help="")
-    storage_collection_cmd.add_argument('--base_dir', default="", help="")
-    storage_collection_cmd.add_argument('--name', default="", help="")
-    storage_collection_cmd.add_argument('--description', default="", help="")
-    
-    # start 子命令
-    storage_start_command = storage_cmd_subparsers.add_parser('start', help='Start Byzer Storage')
-    storage_start_command.add_argument("--file", default=None, required=False, help="")
-    storage_start_command.add_argument("--version", default="0.1.11", required=False, help="")
-    storage_start_command.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    storage_start_command.add_argument('--cluster', default="byzerai_store", help="")
-    storage_start_command.add_argument('--base_dir', default="", help="")
+    storage_collection_cmd.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    storage_collection_cmd.add_argument(
+        "--version", default="0.1.11", required=False, help=""
+    )
+    storage_collection_cmd.add_argument("--cluster", default="byzerai_store", help="")
+    storage_collection_cmd.add_argument("--base_dir", default="", help="")
+    storage_collection_cmd.add_argument("--name", default="", help="")
+    storage_collection_cmd.add_argument("--description", default="", help="")
 
+    # start 子命令
+    storage_start_command = storage_cmd_subparsers.add_parser(
+        "start", help="Start Byzer Storage"
+    )
+    storage_start_command.add_argument("--file", default=None, required=False, help="")
+    storage_start_command.add_argument(
+        "--version", default="0.1.11", required=False, help=""
+    )
+    storage_start_command.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    storage_start_command.add_argument("--cluster", default="byzerai_store", help="")
+    storage_start_command.add_argument("--base_dir", default="", help="")
 
     # stop 子命令
-    storage_stop_command = storage_cmd_subparsers.add_parser('stop', help='Stop Byzer Storage')
+    storage_stop_command = storage_cmd_subparsers.add_parser(
+        "stop", help="Stop Byzer Storage"
+    )
     storage_stop_command.add_argument("--file", default=None, required=False, help="")
-    storage_stop_command.add_argument("--version", default="0.1.11", required=False, help="")
-    storage_stop_command.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    storage_stop_command.add_argument('--cluster', default="byzerai_store", help="")
-    storage_stop_command.add_argument('--base_dir', default="", help="")
-    
+    storage_stop_command.add_argument(
+        "--version", default="0.1.11", required=False, help=""
+    )
+    storage_stop_command.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    storage_stop_command.add_argument("--cluster", default="byzerai_store", help="")
+    storage_stop_command.add_argument("--base_dir", default="", help="")
+
     # export 子命令
-    storage_export_command = storage_cmd_subparsers.add_parser('export', help='Export Byzer Storage Information')    
+    storage_export_command = storage_cmd_subparsers.add_parser(
+        "export", help="Export Byzer Storage Information"
+    )
     storage_export_command.add_argument("--file", default=None, required=False, help="")
-    storage_export_command.add_argument("--version", default="0.1.11", required=False, help="")
-    storage_export_command.add_argument('--ray_address', default="auto", help=locales["help_ray_address"][lang])
-    storage_export_command.add_argument('--cluster', default="byzerai_store", help="")
-    storage_export_command.add_argument('--base_dir', default="", help="")
-    
+    storage_export_command.add_argument(
+        "--version", default="0.1.11", required=False, help=""
+    )
+    storage_export_command.add_argument(
+        "--ray_address", default="auto", help=locales["help_ray_address"][lang]
+    )
+    storage_export_command.add_argument("--cluster", default="byzerai_store", help="")
+    storage_export_command.add_argument("--base_dir", default="", help="")
+
     # Serve 子命令
-    serve_cmd = subparsers.add_parser('serve', help='Serve deployed models with OpenAI compatible APIs')    
-    serve_cmd.add_argument("--file", default="", help="")        
-    serve_cmd.add_argument("--ray_address", default="auto", help="")    
+    serve_cmd = subparsers.add_parser(
+        "serve", help="Serve deployed models with OpenAI compatible APIs"
+    )
+    serve_cmd.add_argument("--file", default="", help="")
+    serve_cmd.add_argument("--ray_address", default="auto", help="")
     serve_cmd.add_argument("--host", default="", help="")
     serve_cmd.add_argument("--port", type=int, default=8000, help="")
     serve_cmd.add_argument("--uvicorn_log_level", default="info", help="")
-    serve_cmd.add_argument("--allow_credentials", action='store_true', help="")
+    serve_cmd.add_argument("--allow_credentials", action="store_true", help="")
     serve_cmd.add_argument("--allowed_origins", default=["*"], help="")
     serve_cmd.add_argument("--allowed_methods", default=["*"], help="")
     serve_cmd.add_argument("--allowed_headers", default=["*"], help="")
@@ -117,6 +202,12 @@ def get_command_args():
     serve_cmd.add_argument("--ssl_keyfile", default="", help="")
     serve_cmd.add_argument("--ssl_certfile", default="", help="")
     serve_cmd.add_argument("--response_role", default="assistant", help="")
-    serve_cmd.add_argument('--template', default="auto", help=locales["help_template"][lang])
+    serve_cmd.add_argument(
+        "--template", default="auto", help=locales["help_template"][lang]
+    )
 
-    return parser.parse_args()
+    if input_args is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(input_args)
+    return args
