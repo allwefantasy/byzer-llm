eb19256faf15d2132037c901d363b3a1b6d46b67
WilliamZhu <allwefantasy@gmail.com>
2024-06-17 21:27:02 +0800

    auto_coder_pre_022_chat_action.yml_225d72dd183f3acadd845852ed2b1edd

--- src/byzerllm/easy_byzerllm_command.py
+++ src/byzerllm/easy_byzerllm_command.py
@@ -1,11 +1,14 @@
+import argparse
 from byzerllm.byzerllm_command import main as byzerllm_main
 
+
 def main():
-    import argparse
-    parser = argparse.ArgumentParser(description='Easy ByzerLLM command line interface')
-    parser.add_argument('command', type=str, help='Command to execute')
-    parser.add_argument('--token', type=str, required=True, help='Token for authentication')
+    parser = argparse.ArgumentParser(description="Easy ByzerLLM command line interface")
+    parser.add_argument("command", type=str, help="Command to execute")
+    parser.add_argument(
+        "--token", type=str, required=True, help="Token for authentication"
+    )
     args = parser.parse_args()
 
-    if args.command == 'start':
-        byzerllm_main(['deploy', '--token', args.token])
+    if args.command == "start":
+        byzerllm_main(["deploy", "--token", args.token])
