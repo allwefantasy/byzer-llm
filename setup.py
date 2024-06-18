ea0e4ae32fbff96829a15b105a37a5fe76a1aad6
WilliamZhu <allwefantasy@gmail.com>
2024-06-18 17:31:18 +0800

    update

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     entry_points={
         'console_scripts': [
             'byzerllm = byzerllm.byzerllm_command:main',
+            'easy-byzerllm = byzerllm.easy_byzerllm_command:main',
         ],
     },
     package_dir={"": "src"},
