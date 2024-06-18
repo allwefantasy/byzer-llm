e766c9afc9ec5ba5341e3d698aa97e6b4d0e3746
WilliamZhu <allwefantasy@gmail.com>
2024-06-18 16:55:22 +0800

    auto_coder_025_chat_action.yml_9ab3e31de1d8b95662098a3c6074badd

--- src/byzerllm/easy_byzerllm_command.py
+++ src/byzerllm/easy_byzerllm_command.py
@@ -1,12 +1,22 @@
+import argparse
 from byzerllm.byzerllm_command import main as byzerllm_main
 
 
 def main():
-    from argparse import ArgumentParser
-    parser = ArgumentParser(description="Easy ByzerLLM command line interface")
-    parser.add_argument("command", type=str, help="Command to execute")
+    parser = argparse.ArgumentParser(description="Easy ByzerLLM command line interface")
+    parser.add_argument("command", type=str, help="Command to execute, e.g. 'start'")
+    parser.add_argument("model", type=str, help="Model name to deploy")
     parser.add_argument("--token", type=str, required=True, help="Token for authentication")
+    parser.add_argument("--ray_address", default="auto", help="Ray cluster address to connect to")
+    
     args = parser.parse_args()
 
     if args.command == "start":
-        byzerllm_main(["deploy", "--token", args.token])
+        byzerllm_main([
+            "deploy",
+            "--model", args.model, 
+            "--token", args.token,
+            "--ray_address", args.ray_address
+        ])
+    else:
+        print(f"Unknown command: {args.command}")
