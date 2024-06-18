9e7b33a2acfd12785cbdd1491761782175040840
WilliamZhu <allwefantasy@gmail.com>
2024-06-18 17:02:42 +0800

    auto_coder_pre_026_chat_action.yml_b4b7858bdab84af749d7a9961d75d7a5

--- src/byzerllm/easy_byzerllm_command.py
+++ src/byzerllm/easy_byzerllm_command.py
@@ -6,16 +6,15 @@ def main():
     parser = argparse.ArgumentParser(description="Easy ByzerLLM command line interface")
     parser.add_argument("command", type=str, help="Command to execute, e.g. 'start'")
     parser.add_argument("model", type=str, help="Model name to deploy")
-    parser.add_argument("--token", type=str, required=True, help="Token for authentication")
+    parser.add_argument("--token", type=str, required=True, help="The model token")
     parser.add_argument("--ray_address", default="auto", help="Ray cluster address to connect to")
     
     args = parser.parse_args()
 
-    if args.command == "start":
+    if args.command == "deploy":
         byzerllm_main([
             "deploy",
-            "--model", args.model, 
-            "--token", args.token,
+            "--model", args.model,             
             "--ray_address", args.ray_address
         ])
     else:
