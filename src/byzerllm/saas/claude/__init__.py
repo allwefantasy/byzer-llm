c54c7e538be5a7a633caa0065dcb897bf9886108
WilliamZhu <allwefantasy@gmail.com>
2024-07-23 13:24:15 +0800

    update

--- src/byzerllm/saas/claude/__init__.py
+++ src/byzerllm/saas/claude/__init__.py
@@ -7,7 +7,12 @@ from typing import List, Dict, Any, Union
 import ray
 from anthropic import Anthropic
 
-from byzerllm.utils.types import BlockVLLMStreamServer, StreamOutputs, SingleOutput, SingleOutputMeta
+from byzerllm.utils.types import (
+    BlockVLLMStreamServer,
+    StreamOutputs,
+    SingleOutput,
+    SingleOutputMeta,
+)
 from byzerllm.utils.langutil import asyncfy_with_semaphore
 
 
@@ -18,28 +23,31 @@ class CustomSaasAPI:
         self.meta = {
             "model_deploy_type": "saas",
             "backend": "saas",
-            "support_stream": True
+            "support_stream": True,
         }
-        other_params = {}        
-        
-        if "saas.base_url" in infer_params:
-            other_params["base_url"] = infer_params["saas.base_url"] 
+        other_params = {}
 
-        self.client = Anthropic(api_key=self.api_key,**other_params)
+        if "saas.base_url" in infer_params:
+            other_params["base_url"] = infer_params["saas.base_url"]
+
+        self.client = Anthropic(api_key=self.api_key, **other_params)
 
         try:
             ray.get_actor("BLOCK_VLLM_STREAM_SERVER")
         except ValueError:
             try:
-                ray.remote(BlockVLLMStreamServer).options(name="BLOCK_VLLM_STREAM_SERVER", lifetime="detached",
-                                                        max_concurrency=1000).remote()
+                ray.remote(BlockVLLMStreamServer).options(
+                    name="BLOCK_VLLM_STREAM_SERVER",
+                    lifetime="detached",
+                    max_concurrency=1000,
+                ).remote()
             except:
-                pass    
+                pass
 
     # saas/proprietary
     def get_meta(self):
         return [self.meta]
-    
+
     async def async_get_meta(self):
         return await asyncfy_with_semaphore(self.get_meta)()
 
@@ -58,7 +66,7 @@ class CustomSaasAPI:
 
         content = []
         for item in ins_json:
-            if ("image" in item or "image_url" in item) and "type" not in item:
+            if "image" in item or "image_url" in item:
                 image_data = item.get("image", item.get("image_url", ""))
                 if not image_data.startswith("data:"):
                     image_data = "data:image/jpeg;base64," + image_data
@@ -67,11 +75,11 @@ class CustomSaasAPI:
                 base64_prefix = ";base64,"
                 if not image_data.startswith(data_prefix):
                     raise ValueError("Invalid image data format")
-                
+
                 format_end = image_data.index(base64_prefix)
-                image_format = image_data[len(data_prefix):format_end]
-                base64_data = image_data[format_end + len(base64_prefix):]    
-                
+                image_format = image_data[len(data_prefix) : format_end]
+                base64_data = image_data[format_end + len(base64_prefix) :]
+
                 content.append(
                     {
                         "type": "image",
@@ -81,29 +89,29 @@ class CustomSaasAPI:
                             "data": base64_data,
                         },
                     }
-                )            
+                )
 
-            if "text" in item and "type" not in item:
+            elif "text" in item and "type" not in item:
                 text_data = item["text"]
                 content.append({"type": "text", "text": text_data})
 
-            if "type" in item and item["type"] in ["text", "image"]:
+            elif "type" in item and item["type"] in ["text"]:
                 content.append(item)
 
         if not content:
             return ins
 
-        return content    
+        return content
 
     async def async_stream_chat(
-            self,
-            tokenizer,
-            ins: str,
-            his: List[dict] = [],
-            max_length: int = 1024,
-            top_p: float = 0.9,
-            temperature: float = 0.1,
-            **kwargs
+        self,
+        tokenizer,
+        ins: str,
+        his: List[dict] = [],
+        max_length: int = 1024,
+        top_p: float = 0.9,
+        temperature: float = 0.1,
+        **kwargs,
     ):
         messages = []
         system_message = ""
@@ -111,7 +119,12 @@ class CustomSaasAPI:
             if message["role"] == "system":
                 system_message = message["content"]
             else:
-                messages.append({"role": message["role"], "content": self.process_input(message["content"])})
+                messages.append(
+                    {
+                        "role": message["role"],
+                        "content": self.process_input(message["content"]),
+                    }
+                )
 
         messages.append({"role": "user", "content": self.process_input(ins)})
 
@@ -121,25 +134,26 @@ class CustomSaasAPI:
 
         if system_message:
             other_params["system"] = system_message
-        
+
         if "stream" in kwargs:
-            other_params["stream"] = kwargs["stream"]        
+            other_params["stream"] = kwargs["stream"]
 
         stream = kwargs.get("stream", False)
 
         try:
-            res_data = await asyncfy_with_semaphore(lambda:self.client.messages.create(                
-                model=self.model,
-                max_tokens=max_length,
-                temperature=temperature,
-                top_p=top_p,
-                messages=messages,                
-                **other_params
-            ))()
+            res_data = await asyncfy_with_semaphore(
+                lambda: self.client.messages.create(
+                    model=self.model,
+                    max_tokens=max_length,
+                    temperature=temperature,
+                    top_p=top_p,
+                    messages=messages,
+                    **other_params,
+                )
+            )()
         except Exception as e:
             traceback.print_exc()
             raise e
-                
 
         if stream:
             server = ray.get_actor("BLOCK_VLLM_STREAM_SERVER")
@@ -147,34 +161,49 @@ class CustomSaasAPI:
 
             def writer():
                 input_tokens = 0
-                r = ""  
-                for response in res_data:                     
-                    
-                    if response.type == "message_start":     
+                r = ""
+                for response in res_data:
+
+                    if response.type == "message_start":
                         request_id[0] = response.message.id
                         input_tokens = response.message.usage.input_tokens
 
-                    if response.type == "content_block_delta":    
-                        v = response.delta.text  
-                        r += v                  
-                        server.add_item.remote(request_id[0],
-                                                    StreamOutputs(outputs=[SingleOutput(text=r, metadata=SingleOutputMeta(
-                                                        input_tokens_count=0,
-                                                        generated_tokens_count=0,
-                                                    ))])
-                                                    )
+                    if response.type == "content_block_delta":
+                        v = response.delta.text
+                        r += v
+                        server.add_item.remote(
+                            request_id[0],
+                            StreamOutputs(
+                                outputs=[
+                                    SingleOutput(
+                                        text=r,
+                                        metadata=SingleOutputMeta(
+                                            input_tokens_count=0,
+                                            generated_tokens_count=0,
+                                        ),
+                                    )
+                                ]
+                            ),
+                        )
                     if response.type == "message_delta":
-                        server.add_item.remote(request_id[0],
-                                                    StreamOutputs(outputs=[SingleOutput(text=r, metadata=SingleOutputMeta(
-                                                        input_tokens_count=input_tokens,
-                                                        generated_tokens_count=response.usage.output_tokens,
-                                                    ))])
-                                                    )
-
+                        server.add_item.remote(
+                            request_id[0],
+                            StreamOutputs(
+                                outputs=[
+                                    SingleOutput(
+                                        text=r,
+                                        metadata=SingleOutputMeta(
+                                            input_tokens_count=input_tokens,
+                                            generated_tokens_count=response.usage.output_tokens,
+                                        ),
+                                    )
+                                ]
+                            ),
+                        )
 
                 server.mark_done.remote(request_id[0])
 
-            threading.Thread(target=writer,daemon=True).start()            
+            threading.Thread(target=writer, daemon=True).start()
 
             time_count = 10 * 100
             while request_id[0] is None and time_count > 0:
@@ -188,7 +217,17 @@ class CustomSaasAPI:
                 return ray.get(server.add_item.remote(request_id[0], "RUNNING"))
 
             await asyncio.to_thread(write_running)
-            return [("", {"metadata": {"request_id": request_id[0], "stream_server": "BLOCK_VLLM_STREAM_SERVER"}})]
+            return [
+                (
+                    "",
+                    {
+                        "metadata": {
+                            "request_id": request_id[0],
+                            "stream_server": "BLOCK_VLLM_STREAM_SERVER",
+                        }
+                    },
+                )
+            ]
 
         time_cost = time.monotonic() - start_time
 
@@ -196,12 +235,19 @@ class CustomSaasAPI:
         generated_tokens_count = res_data.usage.output_tokens
         input_tokens_count = res_data.usage.input_tokens
 
-        return [(generated_text, {"metadata": {
-            "request_id": res_data.id,
-            "input_tokens_count": input_tokens_count,
-            "generated_tokens_count": generated_tokens_count,
-            "time_cost": time_cost,
-            "first_token_time": 0,
-            "speed": float(generated_tokens_count) / time_cost,
-            "stop_reason": res_data.stop_reason
-        }})]
+        return [
+            (
+                generated_text,
+                {
+                    "metadata": {
+                        "request_id": res_data.id,
+                        "input_tokens_count": input_tokens_count,
+                        "generated_tokens_count": generated_tokens_count,
+                        "time_cost": time_cost,
+                        "first_token_time": 0,
+                        "speed": float(generated_tokens_count) / time_cost,
+                        "stop_reason": res_data.stop_reason,
+                    }
+                },
+            )
+        ]
