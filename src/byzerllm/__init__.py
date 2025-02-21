23a76d334784a14a651c1437070e6f979f7f0074
WilliamZhu <allwefantasy@gmail.com>
2025-02-21 11:10:48 +0800

    auto

--- src/byzerllm/__init__.py
+++ src/byzerllm/__init__.py
@@ -470,30 +470,8 @@ class _PrompRunner:
         return False
 
     def to_model(self,result: str):
-        from byzerllm.utils.client import code_utils
-        from byzerllm.utils.nontext import TagExtractor
-                
-        if not isinstance(result, str):
-            raise ValueError("The decorated function must return a string")
-        try:  
-            # quick path for json string  
-            if result.startswith("```json") and result.endswith("```"):
-                json_str = result[len("```json"):-len("```")]
-            else:
-                json_str = code_utils.extract_code(result)[-1][1]
-            json_data = json.loads(json_str)            
-        except json.JSONDecodeError as e:
-            print(f"The returned string is not a valid JSON, e: {str(e)} string: {result}")            
-            tag_extractor = TagExtractor(result)
-            result = tag_extractor.extract()
-            json_data = json.loads(result.content)    
-        
-        try:
-            if isinstance(json_data, list):
-                return [self.model_class(**item) for item in json_data]    
-            return self.model_class(**json_data)
-        except TypeError:
-            raise TypeError("Unable to create model instance from the JSON data")
+        from byzerllm.utils import str2model
+        return str2model.to_model(result, self.model_class)
 
     def run(self, *args, **kwargs):
         func = self.func
